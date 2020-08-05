76606337dfb0f74e413e3c133c9d95da6d91400e
Nico-GS <ng73800@outlook.fr>
2020-08-05 22:23:23 +0200

    Test

---
+++ src/main/java/com/ecommerce/microcommerce/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.ecommerce.microcommerce.controller;
+
+import com.ecommerce.microcommerce.dao.ProductDao;
+import com.ecommerce.microcommerce.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductDao productDao;
+
+    // Produits
+    @GetMapping(value = "Produits")
+    public List<Product> listeProduits() {
+        return productDao.findAll ();
+    }
+
+    // Produits/{id}
+    @GetMapping(value = "Produits/{id}")
+    public Product afficherUnProduit(@PathVariable int id){
+
+        Product product = new Product (id, new String ("aspirateur"), 100);
+        return product;
+
+    }
+    
+}
